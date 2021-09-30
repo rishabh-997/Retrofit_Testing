fb7e9d603c9c235a251d77cf018ace3b3669de6b
Kritik Garg <45820070+KritikGarg1@users.noreply.github.com>
2021-10-01 00:30:00 +0530

    Merge (#12)
    * test
    
    * test
    
    * test
    
    * pr#1
    
    * test2
    
    * pr#3
    
    Co-authored-by: = <kritikgarg12@gmail.com>
    Co-authored-by: Rishabh Agarwal <45125121+rishabh-997@users.noreply.github.com>

--- TestCodes/code.cpp
+++ TestCodes/code.cpp
@@ -18,3 +18,51 @@ class yMyClass {       // The class
     string myString;
     yMyClass()// Attribute (string variable)
 };
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    MyClass()// Attribute (string variable)
+};
+
+class xMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    xMyClass()// Attribute (string variable)
+};
+
+class yMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    yMyClass()// Attribute (string variable)
+};
+
+class xMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    xMyClass()// Attribute (string variable)
+};
+
+class yMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    yMyClass()// Attribute (string variable)
+};
+
+class xMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    xMyClass()// Attribute (string variable)
+};
+
+class yMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    yMyClass()
+};
