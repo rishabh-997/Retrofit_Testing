bab005475e68fc1d633ec6971182c689d2d6f8a3
Kritik Garg <45820070+KritikGarg1@users.noreply.github.com>
2021-10-01 00:13:19 +0530

    Merge (#8)
    * test
    
    * test
    
    * test
    
    * pr#1
    
    Co-authored-by: = <kritikgarg12@gmail.com>

---
+++ TestCodes/code.cpp
@@ -0,0 +1,20 @@
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    MyClass()// Attribute (string variable)
+};
+
+class xMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    xMyClass()// Attribute (string variable)
+};
+
+class yMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    yMyClass()// Attribute (string variable)
+};
