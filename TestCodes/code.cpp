c612f2731dd5a5fc285d7bcdaf2635c8df111e6f
= <kritikgarg123@gmail.com>
2021-10-01 00:23:48 +0530

    test2

--- TestCodes/code.cpp
+++ TestCodes/code.cpp
@@ -18,3 +18,23 @@ class yMyClass {       // The class
     string myString;
     yMyClass()// Attribute (string variable)
 };
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    MyClass()// Attribute (string variable)
+};
+
+class xMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    xMyClass()// Attribute (string variable)
+};
+
+class yMyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+    yMyClass()// Attribute (string variable)
+};
